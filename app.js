818953d3ab2ebb4878a2307765bdd9c7393bab4c
dragonfly95 <dbdyd@nuonchic.com>
2021-01-05 17:21:58 +0900

    add first

--- app.js
+++ app.js
@@ -22,6 +22,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+/*
+var mysql2 = require('mysql2/promise');
+
+var pool = mysql2.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1q2w3e4r5t",
+  database: "gosmsdb",
+  insecureAuth : true
+});
+
+const dbTest = async () => {
+  const connection = await pool.getConnection(async conn => conn);
+  const [rows] = await connection.query("select * from i_deny_numbers limit 0, 10");
+  connection.release();
+  return rows;
+};
+
+dbTest().then((rows) => {
+  rows.forEach(row => {
+    console.log(row);
+  })
+});
+*/
+
+let mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1q2w3e4r5t",
+  database: "gosmsdb"
+});
+connection.connect();
+
+connection.query('select * from i_deny_numbers limit 0, 10', (error, results, fields) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+
+connection.end();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
