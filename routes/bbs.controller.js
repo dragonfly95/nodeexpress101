ee7deef39119378496a58e5a8f7db3c9a24465bc
dragonfly95 <dQzNKCyAgf43e3B>
2021-04-15 23:02:16 +0900

    ccvv

--- routes/bbs.controller.js
+++ routes/bbs.controller.js
@@ -1,13 +1,62 @@
 const models = require('../models');
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    name: Joi.number().integer().min(1900).max(2018)
+});
 
 exports.main = async (req, res, next) => {
-        try {
-            const promise = await models.bbs.findAll();
-            res.json(promise);
+    try {
+        const promise = await models.bbs.findAll();
+        /*
+        const promise = await models.bbs.findAll({
+            attributes: ['id','name'],
+            where: {id: 1}
+        });
+        */
+        res.json(promise);
 
-        } catch (e) {
-            console.error(e);
-            next(e);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
+};
+
+
+exports.create = async (req, res, next) => {
+
+    try {
+        const {error, value} = schema.validate(req.body);
+        if (error != undefined) {
+            throw new Error("invalid error..");
         }
-    };
+        const promise = await models.bbs
+            .create({name: req.body.name, createdAt: new Date()})
+            .then(board => {
+                res.json(board);
+            })
+            .catch(error => {
+                res.error(error);
+            });
 
+            res.json(promise);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
+}
+
+
+exports.get = async (req, res, next) => {
+    try {
+        const promise = await models.bbs.findAll({
+            attributes: ['id','name'],
+            where: {id: req.params.id}
+        });
+        res.json(promise);
+
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
+};
--- routes/bbs.js
+++ routes/bbs.js
@@ -6,4 +6,7 @@ const controller = require('./bbs.controller');
 
 
 router.get("/", controller.main)
+router.post("/", controller.create)
+
+router.get("/:id", controller.get)
 module.exports = router;
